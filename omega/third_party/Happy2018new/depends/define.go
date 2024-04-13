8e8736a666fc2c3449ea756d872e05959ec8c7de
Happy2018new <chenbo79800@163.com>
2024-04-13 20:46:49 +0800

    minor changes on state change struct
    omega related

--- omega/third_party/Happy2018new/depends/define.go
+++ omega/third_party/Happy2018new/depends/define.go
@@ -1,5 +1,22 @@
 package Happy2018new_depends
 
+import "sync"
+
+// ------------------------- general -------------------------
+
+// 一个简单化的实现，
+// 用于向其他线程通知状态更改
+type StateChange struct {
+	// 一个集合，
+	// 其中每一个管道都对应一个线程。
+	// 当发生状态更改，
+	// 每一个管道都将收到信息
+	signal []chan struct{}
+	// 互斥锁，
+	// 用于防止对 signal 的并发读写
+	lock sync.Mutex
+}
+
 // ------------------------- player position -------------------------
 
 // 描述玩家的游戏名称
